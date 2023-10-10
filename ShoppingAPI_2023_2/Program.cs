263f3ce615b78330e89a950c1307d54fd0f23d1f
Jhon <jhonlaurens@gmail.com>
2023-10-09 22:48:20 -0500

    ok

--- ShoppingAPI_2023_2/Program.cs
+++ ShoppingAPI_2023_2/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//Conexion a las base de datos
+//Conexion a las base de datos 
 builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
